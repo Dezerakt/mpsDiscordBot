7d20d63857a657e1256bfc0e0440bdaf64d19f5d
Alan Isaev <alan.isaev@kmf.kz>
2025-09-26 18:32:41 +0500

    set thread timeout to 10, and disabled slowmode

--- internal/actions/createThread.go
+++ internal/actions/createThread.go
@@ -5,9 +5,9 @@ import "github.com/bwmarrin/discordgo"
 func CreateThread(s *discordgo.Session, m *discordgo.MessageCreate) error {
 	_, err := s.MessageThreadStartComplex(m.ChannelID, m.ID, &discordgo.ThreadStart{
 		Name:                m.Author.Username,
-		AutoArchiveDuration: 60,
+		AutoArchiveDuration: 10,
 		Invitable:           false,
-		RateLimitPerUser:    10,
+		RateLimitPerUser:    0,
 	})
 	if err != nil {
 		return err
